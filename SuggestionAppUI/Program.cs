bfa9c03306ba6ddc7354b139f7a09f8dd3b1a4c6
Adithya Vijay K <adithyavj@gmail.com>
2022-01-27 11:56:43 +0530

    Configured DI - Moved dependency injection configuration from program.cs to an extension method

--- SuggestionAppUI/Program.cs
+++ SuggestionAppUI/Program.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using SuggestionAppUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.ConfigureServices();
 
 var app = builder.Build();
 
---
+++ SuggestionAppUI/RegisterServices.cs
@@ -0,0 +1,11 @@
+﻿namespace SuggestionAppUI;
+
+public static class RegisterServices
+{
+    // Extension method for extending the dependency injection services
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+    }
+}
