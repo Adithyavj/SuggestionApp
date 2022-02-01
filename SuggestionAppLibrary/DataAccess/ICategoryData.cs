bc12d0c626b2b2b08a1daad3b9f2693d54d08f78
Adithya Vijay K <adithyavj@gmail.com>
2022-02-01 20:42:46 +0530

    Added db operations for category, add caching, interface for DI.

---
+++ SuggestionAppLibrary/DataAccess/ICategoryData.cs
@@ -0,0 +1,8 @@
+﻿
+namespace SuggestionAppLibrary.DataAccess;
+
+public interface ICategoryData
+{
+    Task CreateCategory(CategoryModel category);
+    Task<List<CategoryModel>> GetAllCategories();
+}
---
+++ SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace SuggestionAppLibrary.DataAccess;
+
+public class MongoCategoryData : ICategoryData
+{
+    private readonly IMongoCollection<CategoryModel> _categories;
+    private readonly IMemoryCache _cache;
+    private const string cacheName = "CategoryData";
+
+    public MongoCategoryData(IDbConnection db, IMemoryCache cache)
+    {
+        _cache = cache;
+        _categories = db.CategoryCollection;
+    }
+
+    // Get all categories
+    public async Task<List<CategoryModel>> GetAllCategories()
+    {
+        // obtain data from cache
+        var output = _cache.Get<List<CategoryModel>>(cacheName);
+        if (output is null)
+        {
+            var results = await _categories.FindAsync(_ => true);
+            output = results.ToList();
+            // putting value in cache (with a timespan of 1 day, data will be cached for a single day)
+            _cache.Set(cacheName, output, TimeSpan.FromDays(1));
+        }
+        return output;
+    }
+
+    // Create a category
+    public Task CreateCategory(CategoryModel category)
+    {
+        return _categories.InsertOneAsync(category);
+    }
+}
