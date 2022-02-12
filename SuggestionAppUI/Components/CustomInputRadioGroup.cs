eb523fa914f514e5fdf2c2e12c391058e406ef2b
Adithya Vijay K <adithyavj@gmail.com>
2022-02-12 19:33:07 +0530

    Created custom component for inputradiogroup

---
+++ SuggestionAppUI/Components/CustomInputRadioGroup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace SuggestionAppUI.Components;
+
+public class CustomInputRadioGroup<TValue> : InputRadioGroup<TValue>
+{
+    private string _name;
+    private string _fieldClass;
+
+    protected override void OnParametersSet()
+    {
+        var fieldClass = EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+        if (fieldClass != _fieldClass || Name != _name)
+        {
+            _fieldClass = fieldClass;
+            _name = Name;
+            base.OnParametersSet();
+        }        
+    }
+}
