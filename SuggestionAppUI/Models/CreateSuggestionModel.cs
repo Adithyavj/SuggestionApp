5a5269e22c2b19850fe105b62b7c68ebce64179d
Adithya Vijay K <adithyavj@gmail.com>
2022-02-12 19:16:06 +0530

    Added create suggestion page logic

---
+++ SuggestionAppUI/Models/CreateSuggestionModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionAppUI.Models;
+
+public class CreateSuggestionModel
+{
+    [Required]
+    [MaxLength(75)]
+    public string Suggestion { get; set; }
+    [Required]
+    [MinLength(1)]
+    [Display(Name ="Category")]
+    public string CategoryId { get; set; }
+    [MaxLength(500)]
+    public string Description { get; set; }
+}
